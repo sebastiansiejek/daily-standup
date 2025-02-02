facd83fcb16101aebaa9e5e6721a3b6b37980b0e
Sebastian Siejek <siejeksebastian@gmail.com>
2025-02-03 00:08:08 +0100

    feat: display link to jira

--- index.ts
+++ index.ts
@@ -5,14 +5,20 @@ import {getJiraTasks} from "@src/api/getJiraTasks.js";
 const lastTimeEntries = await getLastTimeEntries()
 const timeEntriesValues = Object.values(lastTimeEntries)
 
+function getJiraIssueUrl(JIRA_DOMAIN: string, issueKey: string): string {
+  return `https://${JIRA_DOMAIN}/browse/${issueKey}`;
+}
+
 if (timeEntriesValues.length > 0) {
   const jiraTasks = await getJiraTasks(Object.keys(lastTimeEntries))
 
   Object.values(lastTimeEntries).forEach(({task: {name}, duration, tags = []}) => {
     const tagNames = tags.map(tag => tag.name).join(', ')
     const formattedDuration = formatDurationFromSeconds(duration)
-    const status = jiraTasks.data.issues.find(issue => name.includes(issue.key))?.fields.status?.name || 'Unknown'
+    const jiraIssue = jiraTasks.data.issues.find(issue => name.includes(issue.key))
+    const status = jiraIssue?.fields.status?.name || ''
+    const linkToIssue = jiraIssue?.key ? getJiraIssueUrl(process.env.JIRA_DOMAIN, jiraIssue.key) : ''
 
-    console.log(name, tagNames, formattedDuration, status)
+    console.log(name, tagNames, formattedDuration, status, linkToIssue)
   })
 }
