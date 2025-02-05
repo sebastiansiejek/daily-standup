865167dd7f9917900af0d54b713c078d36ba3071
Sebastian Siejek <siejeksebastian@gmail.com>
2025-02-05 14:00:39 +0100

    fix: formating start date

--- src/services/getLastTimeEntries.ts
+++ src/services/getLastTimeEntries.ts
@@ -5,18 +5,15 @@ import type {TimeEntries, TimeEntry} from "@src/shared/types/timeEntries.types.j
 const today = dayjs();
 
 const getStartDate = () => {
-  const today = dayjs();
   const dayOfWeek = today.day()
 
   switch (dayOfWeek) {
-    case 6:
-      return today.subtract(2, "day");
     case 0:
       return today.subtract(3, "days");
     case 1:
       return today.subtract(4, "days");
     default:
-      return today.subtract(1, "days");
+      return today.subtract(2, "days");
   }
 }
 
